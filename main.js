9c34e64424fc96b647f50c32658de73b0f42bb8e
azoozalharte <zezo50115@protonmail.com>
2022-01-09 11:00:38 +0300

    clear button and rgba colors and some ui added

--- main.js
+++ main.js
@@ -1,30 +1,53 @@
 const container = document.querySelector('.container')
-let rows = 20
-let columns = 20
+const clearButton = document.querySelector('#clear-button')
+
+
+let rows = 16
+let columns = 16
 let totalGrids = rows * columns;
+
 container.setAttribute('style', `grid-template-columns: repeat(${columns}, auto); grid-template-rows: repeat(${rows}, auto);`)
 
 
-function createGridDivs() {
-    const gridDiv = document.createElement('div')
-    gridDiv.setAttribute('style', 'border-bottom: 1px solid #C7B198;border-right: 1px solid #C7B198; width: 100%; height: 100%;')
-    gridDiv.classList.add('all-grid')
-    container.appendChild(gridDiv)
+function createGridDivs(totalGrids1) {
+    // if you need border : border-bottom: 1px solid #C7B198;border-right: 1px solid #C7B198;
+    let i = 0;
+    while (i < totalGrids1) {
+        const gridDiv = document.createElement('div')
+        gridDiv.setAttribute('style', 'width: 100%; height: 100%;')
+        gridDiv.classList.add('all-grid')
+        container.appendChild(gridDiv)
+        i++
+    }
 }
 
-let i = 0;
-while (i < totalGrids) {
-    createGridDivs()
-    i++
-}
+createGridDivs(totalGrids)
 const allGrid = document.querySelectorAll('.all-grid')
+
+
+
+function randomRgba() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
 
 function addColorToGrid(grids) {
     grids.forEach(grid => {
         grid.addEventListener('mouseover', () => {
-            grid.setAttribute('style', 'background: black;')
+            grid.setAttribute('style', `background: ${randomRgba()};`)
         })
     });
 }
+
+function clearColors() {
+    allGrid.forEach(grid => {
+        clearButton.addEventListener('click', () => {
+            grid.setAttribute('style', 'background: transparent;')
+        })
+    });
+}
+
+
 
 addColorToGrid(allGrid)
